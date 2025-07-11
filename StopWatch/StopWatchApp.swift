bc6cc48fa183bb2a7cbdf26ec8e9674954fd4717
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-11 23:33:01 +0900

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by maho hayashi on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StopWatchTests/StopWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchTests.swift
+//  StopWatchTests
+//
+//  Created by maho hayashi on 2025/07/11.
+//
+
+import Testing
+@testable import StopWatch
+
+struct StopWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
